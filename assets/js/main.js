1015c20253fdee648ba04707d00411e4b36b67a5
cameocyc <cameo.cyc@gmail.com>
2023-09-03 18:50:22 +0800

    v2
    大改：換成正確資訊、精煉結構、順暢體驗。

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Valera
-* Updated: Jul 27 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/valera-free-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
